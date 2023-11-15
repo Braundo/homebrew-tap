58dfabb1018ba90fcde4a254db8c413df95f4df0
aaron <aaronbraundmeier@gmail.com>
2023-11-15 14:33:05 -0600

    Add files via upload

---
+++ conductor.rb
@@ -0,0 +1,17 @@
+class Conductor < Formula
+  desc "K8s CLI tool that fills gaps in kubectl"
+  homepage "https://github.com/Braundo/conductor"
+  url "https://github.com/Braundo/conductor/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "61cc6cfb165552bce87a90c76f3843969d9bd5ef91517066469f9e883ba21bb8"
+
+  depends_on "go" => :build # Specify dependencies
+
+  def install
+    system "go", "build", "-o", "#{bin}/conductor", "./cmd/conductor"
+  end
+
+  test do
+    system "#{bin}/conductor", "--version" # Replace with a test command
+  end
+end
+
