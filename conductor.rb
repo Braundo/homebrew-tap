407380d937c19d8787daa21a17943f433fefbec6
aaron <aaronbraundmeier@gmail.com>
2023-11-21 16:05:53 -0600

    Update conductor.rb

--- conductor.rb
+++ conductor.rb
@@ -1,8 +1,8 @@
 class Conductor < Formula
   desc "K8s CLI tool that fills gaps in kubectl"
   homepage "https://github.com/Braundo/conductor"
-  url "https://github.com/Braundo/conductor/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "c607145238d6c5740786d27af7f7a682c4168f79089f1f785385a38e6ef3bbd4"
+  url "https://github.com/Braundo/conductor/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "9f5355bb01d8c9cdb4bf9450937aec41e7dbe4c67a95f2fee6e6111caed26146"
 
   depends_on "go" => :build # Specify dependencies
 
